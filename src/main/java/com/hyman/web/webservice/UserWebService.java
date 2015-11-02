e37fb41bafa7fe2ed4cd2e6bca21384fb775e165
Hyman Liu <hymanliu@livenation.com>
2015-11-02 16:43:22 +0800

    swagger  生成doc 文档

--- src/main/java/com/hyman/web/webservice/UserWebService.java
+++ src/main/java/com/hyman/web/webservice/UserWebService.java
@@ -1,6 +1,10 @@
 package com.hyman.web.webservice;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,10 +17,12 @@ import com.hyman.web.bean.User;
 
 
 @Path("/user")
+@Api(tags = {"user"})
 public interface UserWebService {
 
 	@Path("/find")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public User find(@QueryParam("username") String username);
+	@ApiOperation(value="find user by username",tags = {"user"})
+    public User find(@ApiParam @QueryParam("username") String username);
 }
