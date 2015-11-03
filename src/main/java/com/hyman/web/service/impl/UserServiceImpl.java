956b62a6a0e0770c78e2a4c276727e96449c0b1b
Hyman Liu <hymanliu@livenation.com>
2015-11-03 16:55:56 +0800

    Ehcache 缓存同步

--- src/main/java/com/hyman/web/service/impl/UserServiceImpl.java
+++ src/main/java/com/hyman/web/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +30,7 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	@Cacheable(value = "wsCache")
+	@Cacheable(value = "wsCache",key="'userService.list'")
 	public List<User> list() {
 		return userDao.findAll();
 	}
@@ -42,7 +43,9 @@ public class UserServiceImpl implements UserService {
 	
 	@Override
 	@Caching(put={@CachePut(value="wsCache",key="#user.userName + 'userService.find'")
-	,@CachePut(value="wsCache",key="#user.id + 'userService.findById'")})
+	,@CachePut(value="wsCache",key="#user.id + 'userService.findById'")},
+	evict={@CacheEvict(value = "wsCache",key="'userService.list'",condition="#user.userName=='hyman'")}
+	)
 	public User update(User user){
 		 userDao.save(user);
 		 return user;
